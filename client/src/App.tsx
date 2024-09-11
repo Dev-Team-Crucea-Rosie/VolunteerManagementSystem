22141531d60550e6e363ec5b46ae75fc6a570578
CezarBaluta <cezar.baluta@yahoo.com>
2024-09-12 01:11:59 +0300

    [feat]: create route to home page of website

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,21 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages-website/Home';
+//import About from './pages/About';
+//import Contact from './pages/Contact';
+//import UserDetails from './pages/UserDetails'; // New component
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      {/*   <Route path="/about" element={<About />} />*/}
+       {/*  <Route path="/contact" element={<Contact />} />*/}
+        {/* Define dynamic route */}
+     {/*   <Route path="/user/:id" element={<UserDetails />} />*/}
+      </Routes>
     </div>
   );
 }
