0773ffc727fc0d2789a65968dac88cabdac1dad2
calinstefan025 <clavramoniu@gmail.com>
2024-09-19 13:50:48 +0300

    [feat]: create navbar button dropdown

--- client/src/pages-website/components/Navbar.tsx
+++ client/src/pages-website/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { SlArrowDown } from "react-icons/sl";
 import "../../index.css";
 import classes from "../../styles/Navbar.module.css";
 
@@ -11,20 +12,57 @@ const Navbar: React.FC = () => {
           className={`${classes.navbarLogoImg}`}
           src="../../images/LogoCRR.png"
           alt="Logo"
-        />
+        ></img>
         <h5 className={`${classes.navbarLogoText}`}>FILIALA SECTOR 3</h5>
       </div>
       <ul className={`${classes.navbarLinks}`}>
-        <li>
-          <Link to="/about">Despre noi</Link>
+        <li className={`${classes.navbarLink} ${classes.hasDropdown}`}>
+          <div className={classes.navbarLinkButton}>
+            <p>Despre noi</p>
+            <SlArrowDown className={`${classes.navbarLinkArrDown}`} />
+          </div>
+          <ul className={classes.dropdownMenu}>
+            <li>
+              <Link to="/about">Cine suntem</Link>
+            </li>
+            <li>
+              <Link to="/history">Istorie</Link>
+            </li>
+            <li>
+              <Link to="/galery">Galerie media</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/comitet">Comitet CR Sector 3</Link>
+            </li>
+          </ul>
         </li>
-        <li>
-          <Link to="/contact">Activitati & Cursuri</Link>
+        <li className={`${classes.navbarLink} ${classes.hasDropdown}`}>
+          <div className={classes.navbarLinkButton}>
+            <p>Activitati & Cursuri</p>
+            <SlArrowDown className={`${classes.navbarLinkArrDown}`} />
+          </div>
+          <ul className={classes.dropdownMenu}>
+            <li>
+              <Link to="/activities">Prevenire si interventie dezastre</Link>
+            </li>
+            <li>
+              <Link to="/courses">Cursuri de prim ajutor</Link>
+            </li>
+            <li>
+              <Link to="/social">Servicii sociale</Link>
+            </li>
+            <li>
+              <Link to="/foodbank">Banca de alimente</Link>
+            </li>
+          </ul>
         </li>
-        <li>
+        <li className={`${classes.navbarLinkButton}`}>
           <Link to="/app">Voluntariat</Link>
         </li>
-        <li>
+        <li className={`${classes.navbarLinkButton}`}>
           <Link to="/donate" className={`${classes.donateButton}`}>
             Doneaza
           </Link>
