071bfa0c6624f01f2f09c0af78e22e83edb460cc
Avramoniu Calin <clavramoniu@gmail.com>
2024-09-20 20:12:04 +0300

    Revert "[feat]: add initial version of the carousel"
    This reverts commit f2ca1894efea5e4b87520420a4ea1ebe62b08dc1.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 // Get the root element where the app will be rendered
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 
 // Create a root
-const root = createRoot(container!);
+const root = createRoot(container!); 
 
 // Render the app
 root.render(
