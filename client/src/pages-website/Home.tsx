72e63f3e20a8cc6f32ae9aa0d00c5e7d2fdf8dad
Avramoniu Calin <clavramoniu@gmail.com>
2024-09-20 16:26:24 +0300

    [fix]: fix navbar for mobile and redesign with bootstrap

--- client/src/pages-website/Home.tsx
+++ client/src/pages-website/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomNavbar from "./components/Navbar";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Navbar />
+      <CustomNavbar />
     </div>
   );
 };
