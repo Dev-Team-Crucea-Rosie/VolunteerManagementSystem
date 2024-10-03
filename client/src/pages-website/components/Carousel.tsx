b74a2f82576e7276d6546a0af5e4b543251a1c3a
alinandrei2004 <145998080+alinandrei2004@users.noreply.github.com>
2024-10-03 13:27:09 +0300

    [feat]: added fain-in animation to slides' text

--- client/src/pages-website/components/Carousel.tsx
+++ client/src/pages-website/components/Carousel.tsx
@@ -1,19 +1,29 @@
-// Carousel.tsx
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
 import classes from "../../styles/Carousel.module.css";
 
 const CustomCarousel: React.FC = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [fadeInClass, setFadeInClass] = useState(classes.fadeInText);
+
+    const handleSelect = (selectedIndex: number) => {
+        setFadeInClass("");
+        setTimeout(() => {
+            setCurrentSlide(selectedIndex);
+            setFadeInClass(classes.fadeInText);
+        }, 50);
+    };
+
     return (
         <div className={classes.carousel}>
-            <Carousel>
+            <Carousel activeIndex={currentSlide} onSelect={handleSelect} interval={5000}>
                 <Carousel.Item>
                     <img
-                        className={"d-block w-100"}
+                        className="d-block w-100"
                         src="../../images/background.jpg"
                         alt="First slide"
                     />
-                    <Carousel.Caption>
+                    <Carousel.Caption className={fadeInClass}>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
@@ -24,7 +34,7 @@ const CustomCarousel: React.FC = () => {
                         src="../../images/background.jpg"
                         alt="Second slide"
                     />
-                    <Carousel.Caption>
+                    <Carousel.Caption className={fadeInClass}>
                         <h3>Second slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
