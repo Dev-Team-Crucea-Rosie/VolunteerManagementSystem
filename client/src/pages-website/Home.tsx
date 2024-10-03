5f57b458cfd102f73a42a80b4a88a1030c80c1fa
alinandrei2004 <145998080+alinandrei2004@users.noreply.github.com>
2024-10-03 11:08:48 +0300

    [feat]: Carousel added on front page

--- client/src/pages-website/Home.tsx
+++ client/src/pages-website/Home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CustomNavbar from "./components/Navbar";
+import CustomCarousel from "./components/Carousel";
 
 const Home: React.FC = () => {
   return (
     <div>
       <CustomNavbar />
+      <CustomCarousel />
     </div>
   );
 };
