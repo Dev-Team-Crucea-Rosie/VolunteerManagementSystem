c289391d39ee97647c74d7a099e0d94ef38fdc0e
calinstefan025 <clavramoniu@gmail.com>
2024-10-14 19:55:25 +0300

    [fix]: fix gallery footer

--- client/src/pages-website/Gallery.tsx
+++ client/src/pages-website/Gallery.tsx
@@ -4,42 +4,53 @@ import Footer from "./components/Footer";
 import classes from "../styles/Gallery.module.css";
 
 const Galery: React.FC = () => {
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-    const handleImageClick = (imageUrl: string) => {
-        setSelectedImage(imageUrl);
-    };
+  const handleImageClick = (imageUrl: string) => {
+    setSelectedImage(imageUrl);
+  };
 
-    const closeModal = () => {
-        setSelectedImage(null);
-    };
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
 
-    return (
-        <div>
-            <CustomNavbar />
-            <div className="container">
-                <h1 className={classes.galleryTitle}>Galerie</h1>
-                <div className={classes.photoGrid}>
-                    {["../images/background.jpg", "https://via.placeholder.com/300", "https://via.placeholder.com/300", "https://via.placeholder.com/300", "https://via.placeholder.com/300", "https://via.placeholder.com/300"].map((image, index) => (
-                        <div
-                            className={classes.gridItem}
-                            key={index}
-                            onClick={() => handleImageClick(image)}
-                        >
-                            <img src={image} alt={`image ${index + 1}`} />
-                        </div>
-                    ))}
-                </div>
-                {selectedImage && (
-                    <div className={classes.modal} onClick={closeModal}>
-                        <span className={classes.close}>&times;</span>
-                        <img className={classes.modalContent} src={selectedImage} alt="Enlarged" />
-                    </div>
-                )}
-                <Footer />
+  return (
+    <div>
+      <CustomNavbar />
+      <div className="container">
+        <h1 className={classes.galleryTitle}>Galerie</h1>
+        <div className={classes.photoGrid}>
+          {[
+            "../images/background.jpg",
+            "https://via.placeholder.com/300x300",
+            "https://via.placeholder.com/300x300",
+            "https://via.placeholder.com/300x300",
+            "https://via.placeholder.com/300x300",
+            "https://via.placeholder.com/300x300",
+          ].map((image, index) => (
+            <div
+              className={classes.gridItem}
+              key={index}
+              onClick={() => handleImageClick(image)}
+            >
+              <img src={image} alt={`image ${index + 1}`} />
             </div>
+          ))}
         </div>
-    );
+        {selectedImage && (
+          <div className={classes.modal} onClick={closeModal}>
+            <span className={classes.close}>&times;</span>
+            <img
+              className={classes.modalContent}
+              src={selectedImage}
+              alt="Enlarged"
+            />
+          </div>
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Galery;
