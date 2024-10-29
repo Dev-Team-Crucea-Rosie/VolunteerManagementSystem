b29788a20f69cbf7a99f572a94b969f95781590e
Avramoniu Calin <clavramoniu@gmail.com>
2024-10-29 11:56:15 +0200

    [fix]: major cleanup for frontend branch
    removed all backend folders

--- server/routes/verifyToken.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const verifyToken = require('../functions/verifyToken');
-
-router.get('/',verifyToken, (req, res) => {
-    res.json({ message: 'Token is valid' , valid: true});
-});
-
-module.exports = router;
